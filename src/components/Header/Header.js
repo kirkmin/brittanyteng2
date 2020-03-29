c094ce15d4af184bdd86f1f0282361e82ef78acb
Kirk Min <kirk@baller.tv>
2020-03-28 23:23:05 -0700

    basic styling done for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
+import { ReactComponent as HappyFace } from '../../assets/images/happy_face.svg';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">
-        B TENG
+    <header className={styles['header']}>
+      <Link to="/" className={styles['header_link']}>
+        <span className={styles['header_link_circled']}>B</span>
+        <span className={[styles['header_link_circled'], styles['circled--teng']].join(' ')}>TENG</span>
       </Link>
-      <Link to="info">
-        info
-      </Link>
-      <div>:)</div>
+      <Link to="info" className={styles['header_link']}><span className={[styles['header_link_circled'], styles['circled--info']].join(' ')}>Info</span></Link>
+      <div className={styles['header_icon-container']}><HappyFace /></div>
     </header>
   );
 };
